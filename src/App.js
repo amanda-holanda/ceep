bbff249312bdb738aac5cefed195b1512509ade8
Amanda Holanda <amandaholanda_@hotmail.com>
2023-01-10 23:03:43 -0300

    inicialização/limpa/criação do form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <form>
+      <input type="text" placeholder="Título" />
+      <textarea placeholder="Escreva a sua nota..."></textarea>
+      <button>Cria nota</button>
+    </form>
+  );
+}
+
+export default App;
